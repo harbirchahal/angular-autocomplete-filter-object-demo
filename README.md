80c4ef3be28434ee89f9d755a55b8f74d44578ea
harbirchahal <harbirchahal@hotmail.com>
2020-12-25 20:49:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular Autocomplete Filter Object Demo
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './shared';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
